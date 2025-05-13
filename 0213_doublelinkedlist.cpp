b3f1d5239963bfadbbe6e83214e5e045001de3ee
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-05-13 11:21:06 +0700

    method shecdata

--- 0213_doublelinkedlist.cpp
+++ 0213_doublelinkedlist.cpp
@@ -136,27 +136,25 @@ void traverse()
             }
         }
     }
+    void revtraverse()
+	{
+		if (listEmpty())
+			cout << "\nList is emty" << endl;
+		else
+		{
+			cout << "\nRecords in descending order pf roll number are:" << endl;
+			node* currentNode = start;
+			while (currentNode->next != NULL)
+				currentNode = currentNode->next;
+
+			while (currentNode != NULL)
+			{
+				cout << currentNode->noMhs << " " << currentNode->name << endl;
+				currentNode = currentNode->prev;
+			}
+		}
+	}
     
-    void searchData()
-{
-    if (listEmpty() == true)
-    {
-        cout << "\nList is empty" << endl;
-    }
-    node* prev, * curr;
-    prev = curr = NULL;
-    cout << "\nEnter the roll number of the student whose record you want to search: ";
-    int num;
-    cin >> num;
-    if (search(num, &prev, &curr) == false)
-        cout << "\nRecord not found" << endl;
-        else
-        {
-            cout << "\nRecord found" << endl;
-            cout << "\nRoll number: " << curr->noMhs << endl;
-            cout << "\nName: " << curr->name << endl;
-        }
-    }
     int main()
     {
         while (true)
